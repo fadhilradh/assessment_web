7d57fd24bb84ac4135d0d1aabbf4c569511f4f63
Fadhil Radhian <fadhilradhian@gmail.com>
2023-12-25 14:28:56 +0700

    add alert

--- query.php
+++ query.php
@@ -11,25 +11,26 @@ function runQuery()
     $db_conn = mysqli_connect($host, $username, $password, $dbName, $port);
 
     if (mysqli_connect_errno()) {
-        echo "DB Connection Error: " . mysqli_connect_error();
-    } else {
-        $stmt = mysqli_prepare($db_conn, $query);
-
-        if ($stmt) {
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
-
-            $rows = array();
-            while ($r = mysqli_fetch_assoc($result)) {
-                $rows[] = $r;
-            }
-            echo json_encode($rows);
-        } else {
-            echo "Query preparation failed: " . mysqli_error($db_conn);
-        }
-
-        mysqli_close($db_conn);
+        echo '<script>alert("DB Connection Error");</script>';
+        exit(1);
     }
+
+    $stmt = mysqli_prepare($db_conn, $query);
+
+    if ($stmt) {
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+
+        $rows = array();
+        while ($r = mysqli_fetch_assoc($result)) {
+            $rows[] = $r;
+        }
+        echo json_encode($rows);
+    } else {
+        echo "Query preparation failed: " . mysqli_error($db_conn);
+    }
+
+    mysqli_close($db_conn);
 }
 
 runQuery();
