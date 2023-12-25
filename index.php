3dae7d7a2d0489527bc48c49876de8b8d4612150
Fadhil Radhian <fadhilradhian@gmail.com>
2023-12-25 14:34:18 +0700

    add: labels

--- index.php
+++ index.php
@@ -13,12 +13,28 @@
 	<main class="container mx-auto p-4">
 		<h2 class="text-2xl font-bold">MySQL Data</h2>
 		<form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
-			<div class="grid grid-cols-2 gap-4 mt-2 mb-8 rounded-lg">
-				<input type="text" required name="host" class="input" placeholder="MySQL Host" />
-				<input type="text" required name="dbName" class="input" placeholder="MySQL Database" />
-				<input type="text" required name="username" class="input" placeholder="MySQL Username" />
-				<input type="text" required name="password" class="input" placeholder="MySQL Password" />
-				<input type="text" name="port" class="input" placeholder="MySQL Port" />
+			<div class="grid grid-cols-2 gap-4 mt-4 mb-8 rounded-lg">
+				<div>
+					<label for="host" class="block text-sm font-medium text-gray-700">MySQL Host</label>
+					<input type="text" required name="host" id="host" class="input" placeholder="Host" />
+				</div>
+				<div>
+					<label for="dbName" class="block text-sm font-medium text-gray-700">MySQL Database</label>
+					<input type="text" required name="dbName" id="dbName" class="input" placeholder="Database" />
+				</div>
+				<div>
+					<label for="username" class="block text-sm font-medium text-gray-700">MySQL Username</label>
+					<input type="text" required name="username" id="username" class="input" placeholder="Username" />
+				</div>
+				<div>
+					<label for="password" class="block text-sm font-medium text-gray-700">MySQL Password</label>
+					<input type="password" required name="password" id="password" class="input"
+						placeholder="Password" />
+				</div>
+				<div>
+					<label for="port" class="block text-sm font-medium text-gray-700">MySQL Port</label>
+					<input type="text" name="port" id="port" class="input" placeholder="Port" />
+				</div>
 			</div>
 			<h2 class="text-2xl font-bold">MySQL Query</h2>
 			<div class="mt-2 mb-4 rounded-lg">
